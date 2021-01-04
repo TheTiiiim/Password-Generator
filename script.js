96f730622c236ce07e64e203e33b7df2f3122e72
Tim Scott <0timothy.scott0@gmail.com>
2021-01-04 18:54:56 -0500

    connect generator inputs and output to webpage

--- script.js
+++ script.js
@@ -1,17 +1,66 @@
+$(function () {
+	$('#passwordOptions').on("submit", function (e) {
+		e.preventDefault(); // cancel the actual submit
+
+		let options = $('#passwordOptions').serializeArray();
+
+		let optionsObj = {
+			length: 8,
+			lower: false,
+			upper: false,
+			number: false,
+			special: false
+		};
+
+		for (key in options) {
+			if (options[key].name === "length") {
+				optionsObj[options[key].name] = parseInt(options[key].value);
+			} else {
+				optionsObj[options[key].name] = (options[key].value === 'true');
+			}
+		}
+
+		let isValid = true;
+
+		if (isNaN(optionsObj.length)) {
+			replaceResultReceiver("Password length input is Not a Number.");
+			isValid = false;
+		}
+
+		if (!(optionsObj.lower || optionsObj.upper || optionsObj.number || optionsObj.special)) {
+			replaceResultReceiver("Must include some characters.");
+			isValid = false;
+		}
+
+		if (isValid) {
+			let password = generatePassword(optionsObj.length, optionsObj.lower, optionsObj.upper, optionsObj.number, optionsObj.special);
+			replaceResultReceiver(password);
+		}
+	});
+});
+
+function replaceResultReceiver(string) {
+	$("#resultReceiver").text(string);
+}
+
+
 function generatePassword(
-	length = 8,
-	withLowerCharacters = true,
-	withUpperCharacters = false,
-	withNumeralCharacters = false,
-	withSpecialCharacters = false
+	length,
+	withLowerCharacters,
+	withUpperCharacters,
+	withNumeralCharacters,
+	withSpecialCharacters
 ) {
+
 	//character pools
+
 	const lowerCharacters = "abcdefghijklmnopqurstuvwxyz";
 	const upperCharacters = lowerCharacters.toUpperCase();
 	const numeralCharacters = "0123456789";
 	const specialCharacters = ` !"#$%&'()*+,-./:;<=>?@[\\]^_\`{|}~`;
 
 	//instance character pool
+
 	let characterSet = "";
 
 	if (withLowerCharacters) {
@@ -31,6 +80,8 @@ function generatePassword(
 	}
 
 	let result = "";
+
+	// generate password
 
 	for (let i = 0; i < length; i++) {
 		let seed = Math.random();
